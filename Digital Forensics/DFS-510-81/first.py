434a62dcfe3a0fe2fea5f81a73f22efff12353e6
Travis Palmer <palmer.travis@gmail.com>
2018-01-20 17:29:11 -0500

    comment Week 3 homework

--- Digital Forensics/DFS-510-81/first.py
+++ Digital Forensics/DFS-510-81/first.py
@@ -8,9 +8,14 @@
 
 def main():
     testString = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"
+
+    # Capture command-line user input using raw_input
     searchWord = raw_input("Please enter search term: ")
 
+    # If the word is found, save the index to foundIndex
     foundIndex = testString.find(searchWord)
+
+    # If no word was found, find() returns an index of -1
     termFound = "found" if foundIndex != -1 else "not found"
 
     print searchWord, "was", termFound
