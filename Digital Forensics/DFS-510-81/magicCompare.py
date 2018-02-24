4135f88135430f070185f769336e110254de4986
Travis Palmer <palmer.travis@gmail.com>
2018-02-24 12:10:24 -0500

    rough draft of final project

--- Digital Forensics/DFS-510-81/magicCompare.py
+++ Digital Forensics/DFS-510-81/magicCompare.py
@@ -8,7 +8,104 @@ Required System Library: libmagic
 Required Python Library: python-magic
 """
 
+import os
 import magic
+import json
+import argparse
 
-mime = magic.from_file("./capture.raw", mime=True)
-print(mime)
+
+class MagicCompare:
+    """Responsible for magic number comparison business logic."""
+
+    parsedArgs = None
+    extensions = None
+
+    def __init__(self):
+        """Magic number comparison constructor."""
+        self.setupParsedArguments()
+        self.initializeExtensions()
+
+    def perform(self):
+        path = self.parsedArgs.path
+        for filePath in os.listdir(path):
+            fullPath = os.path.join(path, filePath)
+            fileName, fileExtension = os.path.splitext(fullPath)
+            fileExtension = fileExtension.replace('.', '')
+            openedFile = self.validateAndOpenFile(fullPath, 'rb')
+            if not openedFile:
+                continue
+
+            fileContents = self.readFile(openedFile)
+            if fileContents and fileExtension:
+                print("Checking " + fullPath)
+                mime = magic.from_file(fullPath, mime=True)
+                if fileExtension in self.extensions:
+                    foundMIME = self.extensions[fileExtension]["mime"]
+                    if mime != foundMIME:
+                        print("Discrepancy found:")
+                        print("\tMagic detected: " + mime)
+                        print("\tLocal found: " + foundMIME)
+
+    def initializeExtensions(self):
+        """
+        Initialize the extensions dictionary.
+        Source: https://gist.github.com/Qti3e/6341245314bf3513abb080677cd1c93b
+        """
+        openedJSON = self.openFile('./extensions.json', 'r')
+        self.extensions = json.load(openedJSON)
+
+    def setupParsedArguments(self):
+        """Initialize parsedArgs."""
+        parser = argparse.ArgumentParser(
+            'magicCompare'
+        )
+
+        parser.add_argument(
+            "-p",
+            "--path",
+            help="Specify the directory to use for comparison",
+            required=True
+        )
+
+        self.parsedArgs = parser.parse_args()
+
+    def openFile(self, path, mode):
+        """Attempt to open the file and return it."""
+        try:
+            openFile = open(path, mode)
+        except IOError:
+            self.logger.error('Failed to open: ' + path)
+            return
+        else:
+            return openFile
+
+    def validateAndOpenFile(self, path, mode):
+        """If the file is valid, open it and return the opened file"""
+        if self.isValidFile(path):
+            return self.openFile(path, mode)
+        else:
+            return False
+
+    def isValidFile(self, path):
+        """Verify the path is valid, is not a symbolic link, and is real"""
+        if (os.path.exists(path) and
+           not os.path.islink(path) and
+           os.path.isfile(path)):
+            return True
+        else:
+            return False
+
+    def readFile(self, openedFile):
+        """Attempt to read the file contents and return them"""
+        try:
+            contents = openedFile.read()
+        except IOError:
+            openedFile.close()
+            print('Failed to read: ' + openedFile)
+            return
+        else:
+            return contents
+
+
+magicCompare = MagicCompare()
+magicCompare.perform()
